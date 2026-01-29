d96458dcb4b1da899d40bc20c59be2f9b104369d
Gutierrez101 <mateoguti2021info@gmail.com>
2026-01-29 07:30:47 -0500

    Eliminacion de archivo innecesario y correcion del archivo init /detection

--- detection/__init__.py
+++ detection/__init__.py
@@ -4,10 +4,8 @@ Módulo de detección para el sistema de reidentificación.
 
 from .face_detection import FaceDetection
 from .body_detection import BodyDetection
-from .view_classification import ViewClassification
 
 __all__ = [
     'FaceDetection',
-    'BodyDetection',
-    'ViewClassification'
+    'BodyDetection'
 ]
